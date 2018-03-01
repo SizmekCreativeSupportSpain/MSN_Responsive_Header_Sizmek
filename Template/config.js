9f2234b6fd7c46bd0c70d6cc0b547e4dd34df10d
Javier Egido <javier.egido.alonso@gmail.com>
2018-03-01 22:16:20 +0100

    First commit

---
+++ Template/config.js
@@ -0,0 +1,104 @@
+define(
+{
+  "adFormatId": 10366,
+  "name": "DeluxeBanner_2_0_0", 
+  "defaultBanner": "Main_Banner", 
+  "banners": [ 
+    {
+      "name": "Main_Banner",
+      "width": 1,
+      "height": 1,
+      "defaultImage": "images/1x1.jpg"
+    }
+  ],
+  "clickThrough": {
+    "url": "http://www.sizmek.com/?defaultClickthrough",
+    "target": "newWindow",
+    "showMenuBar": true,
+    "showAddressBar": true,
+    "closePanels": true
+  },
+  "variables": [
+    {
+      "key": "mdAdaptiveBreakpoints",
+      "value": "628x162,950x250,1272x328"
+     },
+    {
+      "key": "mdAlignment",
+      "value": "center"
+    },
+    {
+      "key": "mdAnimateResize",
+      "value": "false"
+    },
+    {
+      "key": "mdAspectRatio",
+      "value": "undefined"
+    },
+    {
+      "key": "mdAutoHeightThreshold",
+      "value": "1"
+     },
+    {
+      "key": "mdAutoRepositionInterval",
+      "value": "200"
+    },
+        {
+      "key": "mdBackupImgPath",
+      "value": "images/default.jpg"
+    },
+    {
+      "key": "mdFixedHeight",
+      "value": "0"
+    },
+    {
+      "key": "mdFixedWidth",
+      "value": "0"
+    },
+    {
+      "key": "mdHeightDependency",
+      "value": "advertiserdiv_adaptive"
+     },
+    {
+      "key": "mdMaximumHeight",
+      "value": "328"
+    },
+        {
+      "key": "mdMaximumWidth",
+      "value": "1272"
+    },
+    {
+      "key": "mdMinimumHeight",
+      "value": "162"
+    },
+    {
+      "key": "mdMinimumWidth",
+      "value": "628"
+    },
+    {
+      "key": "mdParentLevelsToResize",
+      "value": "0"
+     },
+    {
+      "key": "mdIsDomainConfigEnabled",
+      "value": "false"
+    },
+    {
+      "key": "mdProgSettingsFolderPath",
+      "value": "//services.serving-sys.com/programmatic/DomainList/"
+    },
+        {
+      "key": "mdResizeBehavior",
+      "value": "adaptive"
+     },
+    {
+      "key": "mdResizeFriendlyIframe",
+      "value": "yes"
+    },
+        {
+      "key": "mdWidthDependency",
+      "value": ".full-width"
+    }
+  ]
+}
+);
